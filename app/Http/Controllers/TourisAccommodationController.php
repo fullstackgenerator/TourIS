e4947742684df76790e97c6045ad13213759a277
fullstackgenerator <simon.hojnik@gmail.com>
2025-03-06 11:18:08 +0100

    added controllers

---
+++ app/Http/Controllers/TourisAccommodationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TourisAccommodation;
+use App\Http\Requests\StoreTourisAccommodationRequest;
+use App\Http\Requests\UpdateTourisAccommodationRequest;
+
+class TourisAccommodationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTourisAccommodationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TourisAccommodation $tourisAccommodation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TourisAccommodation $tourisAccommodation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTourisAccommodationRequest $request, TourisAccommodation $tourisAccommodation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TourisAccommodation $tourisAccommodation)
+    {
+        //
+    }
+}
